d7c8340b57451c7913328ee66cba54860598712d
김정욱 <yeoback07@icloud.com>
2024-09-02 08:59:10 +0900

    mod1

--- commands/utility/setup.js
+++ commands/utility/setup.js
@@ -1,3 +1,8 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('setup')
+        .setDescription('Setup a custom command')
 
 }
